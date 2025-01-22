9214535082c247495e149da4ab9fa86de2b18456
Daniel Dias <ecodelearn@outlook.com>
2025-01-22 08:09:13 -0300

    Explicação de Condição Simples

---
+++ ModuloD/aula11/exercicio008.js
@@ -0,0 +1,6 @@
+let vel = 12.5
+console.log(`A velocidade do carro é ${vel} km/h.`)
+if(vel > 60) { // Condição simples
+    console.log(`Você ultrapassou a velocidade permitida de ${vel} km/h. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
