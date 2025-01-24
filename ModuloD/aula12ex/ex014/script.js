60d05e78abac4815b924de37f4dd2c65529e305a
Daniel Dias <ecodelearn@outlook.com>
2025-01-24 08:53:45 -0300

    Resulução do ex014, site de fotos

--- ModuloD/aula12ex/ex014/script.js
+++ ModuloD/aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert("Olá")
+function carregar() {
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem')
+    let data = new Date()
+    // let hora = 19
+    let hora = data.getHours()
+    let minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora}:${minuto} horas.`
+    if (hora >= 0 && hora < 12) {
+        // bom dia!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#f4ddbe'
+    } else if (hora >= 12 && hora < 18) {
+        // boa tarde!
+        img.src = 'fototarde.png'
+        document.body.style.background = '#f5a964'
+    } else {
+        // boa noite! 
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#435c7d'
+        
+    }
+}
